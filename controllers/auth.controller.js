650b3492266bcdd7f13ec2d7297c8400357e070f
tinh102001 <tinhduc312001@gmail.com>
2023-02-13 09:31:16 +0700

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,10 +3,117 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const authCtrl = {
-  register: async (req, res) => {},
-  login: async (req, res) => {},
-  logout: async (req, res) => {},
-  generateAccessToken: async (req, res) => {},
+  register: async (req, res) => {
+    try {
+      const { fullname, username, email, password, gender } = req.body;
+
+      const userName = await Users.findOne({ username });
+      if (userName)
+        return res.status(400).json({ msg: "Tên người dùng đã tồn tại." });
+
+      const userEmail = await Users.findOne({ email });
+      if (userEmail)
+        return res.status(400).json({ msg: "Email này đã tồn tại." });
+
+      if (password.length < 6)
+        return res
+          .status(400)
+          .json({ msg: "Mật khẩu phải có ít nhất 6 kí tự." });
+
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      const newUser = new Users({
+        fullname,
+        username,
+        email,
+        password: passwordHash,
+        gender,
+      });
+
+      const accessToken = createAccessToken({ id: newUser._id });
+      const refreshToken = createRefreshToken({ id: newUser._id });
+
+      res.cookie("refreshtoken", refreshToken, {
+        httpOnly: true,
+        path: "/api/refresh_token",
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+      });
+
+      await newUser.save();
+
+      res.json({
+        msg: "Đăng kí thành công!",
+        accessToken,
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await Users.findOne({ email });
+
+      if (!user)
+        return res.status(400).json({ msg: "Email này không tồn tại." });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res.status(400).json({ msg: "Mật khẩu không chính xác." });
+
+      const accessToken = createAccessToken({ id: user._id });
+      const refreshToken = createRefreshToken({ id: user._id });
+
+      res.cookie("refreshtoken", refreshToken, {
+        httpOnly: true,
+        path: "/api/refresh_token",
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+      });
+
+      res.json({
+        msg: "Đăng nhập thành công!",
+        accessToken,
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  logout: async (req, res) => {
+    try {
+      res.clearCookie("refreshtoken", { path: "/api/refresh_token" });
+      return res.json({ msg: "Đăng xuất thành công!" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  generateAccessToken: async (req, res) => {
+    try {
+      const refreshToken = req.cookies.refreshtoken;
+      if (!refreshToken) return res.status(400).json({ msg: "Xin hãy đăng nhập." });
+
+      jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        async (err, result) => {
+          if (err) return res.status(400).json({ msg: "Xin hãy đăng nhập." });
+
+          const user = await Users.findById(result.id).select("-password");
+
+          if (!user)
+            return res.status(400).json({ msg: "This does not exist." });
+
+          const accessToken = createAccessToken({ id: result.id });
+
+          res.json({
+            accessToken,
+          });
+        }
+      );
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 const createAccessToken = (payload) => {
