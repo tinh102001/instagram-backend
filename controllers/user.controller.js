81cc71c229f2c959844bef686e5708a8d3b665cf
tinh102001 <tinhduc312001@gmail.com>
2023-02-20 09:25:57 +0700

    #3 getUser, updateUser, searchUser, follow, unfollow done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,108 @@
 import { Users } from "../models/user.model.js";
 
 export const userCtrl = {
-  searchUser: async (req, res) => {},
-  getUser: async (req, res) => {},
-  updateUser: async (req, res) => {},
-  follow: async (req, res) => {},
-  unfollow: async (req, res) => {},
+  searchUser: async (req, res) => {
+    try {
+      const users = await Users.find({
+        username: { $regex: req.query.username },
+      })
+        .limit(10)
+        .select("fullname username avatar");
+
+      res.json({ users });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  getUser: async (req, res) => {
+    try {
+      const user = await Users.findById(req.params.id).select("-password");
+
+      if (!user)
+        return res.status(400).json({ msg: "Người dùng không tồn tại." });
+
+      res.json({ user });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const { avatar, fullname, mobile, address, story, website, gender } =
+        req.body;
+      if (!fullname)
+        return res.status(400).json({ msg: "Please add your full name." });
+
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          avatar,
+          fullname,
+          mobile,
+          address,
+          story,
+          website,
+          gender,
+        }
+      );
+
+      res.json({ msg: "Cập nhật thành công!" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  follow: async (req, res) => {
+    try {
+      const user = await Users.find({
+        _id: req.params.id,
+        followers: req.user._id,
+      });
+      if (user.length > 0)
+        return res.status(500).json({ msg: "Bạn đã theo dõi người này rồi." });
+
+      const newUser = await Users.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $push: { followers: req.user._id },
+        },
+        { new: true }
+      ).populate("followers following", "-password");
+
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $push: { following: req.params.id },
+        },
+        { new: true }
+      );
+
+      res.json({ newUser });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  unfollow: async (req, res) => {
+    try {
+      const newUser = await Users.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $pull: { followers: req.user._id },
+        },
+        { new: true }
+      ).populate("followers following", "-password");
+
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $pull: { following: req.params.id },
+        },
+        { new: true }
+      );
+
+      res.json({ newUser });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
   suggestionsUser: async (req, res) => {},
 };
