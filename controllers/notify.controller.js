5dc50c778125a2bbe617ed9883f74772994bf880
tinh102001 <tinhduc312001@gmail.com>
2023-02-21 20:45:21 +0700

    #5 Do createNotify, getNotify, removeNotify, isReadNotify, deleteAllNotifies done

--- controllers/notify.controller.js
+++ controllers/notify.controller.js
@@ -1,9 +1,72 @@
 import { Notifies } from "../models/notify.model.js";
 
 export const notifyCtrl = {
-  createNotify: async (req, res) => {},
-  removeNotify: async (req, res) => {},
-  getNotifies: async (req, res) => {},
-  isReadNotify: async (req, res) => {},
-  deleteAllNotifies: async (req, res) => {},
+  createNotify: async (req, res) => {
+    try {
+      const { id, recipients, url, text, content, image } = req.body;
+
+      if (recipients.includes(req.user._id.toString())) return;
+
+      const notify = new Notifies({
+        id,
+        recipients,
+        url,
+        text,
+        content,
+        image,
+        user: req.user._id,
+      });
+
+      await notify.save();
+      return res.json({ notify });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  getNotifies: async (req, res) => {
+    try {
+      const notifies = await Notifies.find({ recipients: req.user._id })
+        .sort("-createdAt")
+        .populate("user", "avatar username");
+
+      return res.json({ notifies });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  removeNotify: async (req, res) => {
+    try {
+      const notify = await Notifies.findOneAndDelete({
+        id: req.params.id,
+        url: req.query.url,
+      });
+
+      return res.json({ notify });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  isReadNotify: async (req, res) => {
+    try {
+      const notifies = await Notifies.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          isRead: true,
+        }
+      );
+
+      return res.json({ notifies });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  deleteAllNotifies: async (req, res) => {
+    try {
+      const notifies = await Notifies.deleteMany({ recipients: req.user._id });
+
+      return res.json({ notifies });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
